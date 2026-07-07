a5bf4350950cd3b4422b2aa904938443464e71fd
discord <amoedolorenzobrais@gmail.com>
2026-07-07 19:12:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-version: 2
-updates:
-  - package-ecosystem: "npm" 
-    directory: "/" #
-    schedule:
-      interval: "daily"
+const fs = require('fs');
+const { createBot } = require('./src/bot.js');
+
+if (!fs.existsSync('./setting.json')) {
+    console.error('Error: setting.json file not found!');
+    process.exit(1);
+}
+
+const settings = JSON.parse(fs.readFileSync('./setting.json', 'utf-8'));
+createBot(settings);
